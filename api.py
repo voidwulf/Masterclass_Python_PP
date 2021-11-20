6ce77ef414e41bd037a746084fc1e979ec2d3f19
Martin Starčević <martin.starcevic989@gmail.com>
2021-11-20 14:15:19 +0000

    import requests and create extract (self) function

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
+import requests #FYI: have import request in order to reqest to run in request.get()
 class API:
     def extract (self):
-        pass 
+        data = requests.get('https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0') #rquest data from API
+        return data.text #return data in text forma
+
     def main(self):
-        print("Hello World")
+        return print(self.extract()) # print extracted data
 
 
 api = API()
